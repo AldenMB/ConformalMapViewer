4bb62da45cbf99f37baafae3d8e759ca802d2fe2
Alden Bradford <aldenbradford@gmail.com>
2020-05-21 19:23:53 -0400

    grid drawing routines

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+import * as draw from './modules/draw.js';
+
 window.onload = function() {
 	let domain = document.getElementById("domain_canvas");
 	let codomain = document.getElementById("codomain_canvas");
-	console.log(math.sqrt(-4).toString());
+	document.getElementById('domain_clear').onclick = function(){
+		draw.clear(domain);
+	}
+	document.getElementById('codomain_clear').onclick = function(){
+		draw.clear(codomain);
+	}
+	document.getElementById('domain_grid').onclick = function(){
+		draw.grid(domain);
+	}
+	document.getElementById('codomain_grid').onclick = function(){
+		draw.grid(codomain);
+	}
+	document.getElementById('domain_axes').onclick = function(){
+		draw.axes(domain);
+	}
+	document.getElementById('codomain_axes').onclick = function(){
+		draw.axes(codomain);
+	}
+	
 }
---
+++ modules/draw.js
@@ -0,0 +1,41 @@
+function axes(canvas, {lineWidth = 2, color = "blue"}={}) {
+	const {width, height} = canvas.getBoundingClientRect();
+	const ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.lineWidth = lineWidth;
+	ctx.strokeStyle = color;
+	ctx.moveTo(width/2, 0);
+	ctx.lineTo(width/2, height);
+	ctx.moveTo(0, height/2);
+	ctx.lineTo(width, height/2);
+	ctx.stroke();
+}
+
+function grid(canvas, {spacing = 25, lineWidth = 1, color = "lightblue"}={}) {
+	const {width, height} = canvas.getBoundingClientRect();
+	const ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.lineWidth = lineWidth;
+	ctx.strokeStyle = color;
+	for(let x=0; x < width/2; x += spacing){
+		ctx.moveTo(width/2+x,0);
+		ctx.lineTo(width/2+x,height);
+		ctx.moveTo(width/2-x,0);
+		ctx.lineTo(width/2-x,height);
+	}
+	for(let y=0; y < height/2; y += spacing){
+		ctx.moveTo(0,height/2+y);
+		ctx.lineTo(width,height/2+y);
+		ctx.moveTo(0,height/2-y);
+		ctx.lineTo(width,height/2-y);
+	}
+	ctx.stroke();
+}
+
+function clear(canvas) {
+	const {width, height} = canvas.getBoundingClientRect();
+	const ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0,width, height);
+}
+
+export {axes, grid, clear};
