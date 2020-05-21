d33c87e67304f0b424ef765f4234741a2b13cab7
Alden Bradford <aldenbradford@gmail.com>
2020-05-21 17:14:04 -0400

    create files for development

---
+++ main.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	let domain = document.getElementById("domain_canvas");
+	let codomain = document.getElementById("codomain_canvas");
+	console.log(math.sqrt(-4).toString());
+}
