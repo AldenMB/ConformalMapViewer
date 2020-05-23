73bdf609078cbcd03b9ddfee5b6c12b37019cdc7
Alden Bradford <aldenbradford@gmail.com>
2020-05-23 15:38:48 -0400

    partial progress on voronoi plotting

--- modules/transform.js
+++ modules/transform.js
@@ -77,13 +77,36 @@ function image_draw(func, domain, codomain, domain_unit = 100, codomain_unit = 1
 	cctx.putImageData(codomain_data, 0, 0);
 }
 
+/*
+function voronoi_fill(sites, canvas){
+	const {width, height} = canvas;
+	const ctx = canvas.getContext("2d");
+	const voronoi = new Voronoi();
+	const diagram = voronoi.compute(sites, {xl:0, xr:width, yt:0, yb:height});
+	console.log(diagram.execTime);
+	for(let cell of diagram.cells){
+		const points = cell.halfedges.map(he => he.getStartpoint());
+		const {x:startx, y:starty} = points.pop();
+		ctx.fillStyle = "green"//site.color_data;
+		ctx.beginPath();
+		ctx.moveTo(startx, starty);
+		for(let point of points){
+			const {x, y} = point;
+			ctx.lineTo(x,y);
+		}
+		ctx.closePath();
+		ctx.fill();
+	}
+	
+}
+
+
 function voronoi_image(func, domain, codomain, domain_unit = 100, codomain_unit = 100){
 	const {width:dwidth, height:dheight} = domain;
 	const dctx = domain.getContext("2d");
 	const {width:cwidth, height:cheight} = codomain;
 	const cctx = codomain.getContext("2d");
 	const domain_data = dctx.getImageData(0, 0, dwidth, dheight);
-	const codomain_data = cctx.createImageData(cwidth, cheight);
 	const image_points = [];
 	for(let dx=0; dx<dwidth; ++dx){
 		for(let dy=0; dy<dheight; ++dy){
@@ -95,21 +118,17 @@ function voronoi_image(func, domain, codomain, domain_unit = 100, codomain_unit 
 				domain_unit, 
 				codomain_unit
 			);
-			if(0<= cx && cx<cwidth && 0<=cy && cy<cwidth){
-				const di = 4*(dx+dwidth*dy);
-				image_points.push({
-					x: cx,
-					y: cy,
-					r: domain_data.data[di],
-					g: domain_data.data[di+1],
-					b: domain_data.data[di+2],
-					a: domain_data.data[di+3]
-				});
-			}
+			//if(cx<0 || cx>cwidth || cy<0 || cy>cheight){continue};
+			const di = 4*(dx+dwidth*dy);
+			image_points.push({
+				x: cx,
+				y: cy//,
+				//color_data: "rgba("+domain_data.data.slice(di,di+4).join()+")"
+			});
 		}
 	}
-	codomain_data.data = voronoi_fill(image_points, cwidth, cheight);
-	cctx.putImageData(codomain_data, 0, 0);	
+	voronoi_fill(image_points, codomain);
 }
+*/
 
 export {preimage, image_draw}
